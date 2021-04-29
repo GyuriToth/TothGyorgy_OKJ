8b942d231271015e83db63333965f771e9a5e259
GyuriToth <m.toth.gyuri9@gmail.com>
2021-04-29 12:20:17 +0200

    shop filterbar

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/App.css';
 import Product from './Product.js';
 import { ProductArray } from './ProductArray'
 import { CategoryArray } from './CategoryArray'
+import { BottomNavigation } from '@material-ui/core';
 
 function Shop() {
   const products = ProductArray()
   const category = CategoryArray()
 
   const [selectedCategory, setSelectedCategory] = useState('0')
+  const [searchKeyWord, setSearchKeyWord] = useState ("")
+  const [order, setOrder] = useState ("0")
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOrder("ascending")
+    }, 1500)
+  },[])
+
+ const orderByPrice = (e) => {
+    setOrder(e);
+      products.sort((a,b) => {
+          if (order==="ascending"){
+            if (a.price > b.price){
+              return -1;
+            }
+            else return 1;
+          }
+    
+          if (order==="descending"){
+            if (a.price < b.price){
+              return -1;
+            }
+            else return 1;
+          }
+      })
+    console.log("termekek", products);
+  }
 
   return (
+
+
     <div className="shop">
       <div className="shop__container">
         <div className="shop__filterBar">
@@ -29,11 +60,11 @@ function Shop() {
                     )
                   }
               </select>
-              <select className="shop__filterSelect">
-                <option value="asc">Ár szerint növekvő</option>
-                <option value="desc">Ár szerint csökkenő</option>
+              <select value={order} className="shop__filterSelect" onChange={(e) => orderByPrice(e.target.value)}>
+                <option value="ascending">Ár szerint növekvő</option>
+                <option value="descending">Ár szerint csökkenő</option>
               </select>
-              <input type="text" className="shop__filterSearch"></input>
+              <input type="text" className="shop__filterSearch" onChange={(e) => setSearchKeyWord(e.currentTarget.value)}></input>
             </div>
           </div>
         
@@ -43,17 +74,34 @@ function Shop() {
           <div className="shop__row">
             {
               selectedCategory === "0" ?
-
-              products.map(product=>
-                <Product 
-                  key={product.id}
-                  id={product.id}
-                  title={product.title}
-                  price={product.price}
-                  quantity={product.quantity}
-                  image={product.image}
-                  desc={product.desc}
-                />
+              
+              (
+                searchKeyWord === "" ?
+                  products.map(product=>
+                    <Product 
+                      key={product.id}
+                      id={product.id}
+                      title={product.title}
+                      price={product.price}
+                      quantity={product.quantity}
+                      image={product.image}
+                      desc={product.desc}
+                    />
+                  )
+                  :
+                  (
+                    products.filter( item => item.title.toUpperCase().includes(searchKeyWord.toUpperCase()) ).map(product=>
+                      <Product 
+                          key={product.id}
+                          id={product.id}
+                          title={product.title}
+                          price={product.price}
+                          quantity={product.quantity}
+                          image={product.image}
+                          desc={product.desc}
+                      />
+                    )
+                  )
               )
               :
               products.filter( item => item.category===selectedCategory ).map(product=>
